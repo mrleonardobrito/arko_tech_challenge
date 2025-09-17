f5548d3abd14693529846d9948cacd0f55bf43dc
mrleonardobrito <lbritogit@gmail.com>
2025-09-17 10:27:28 -0300

    solving url path problem

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # Catch-all pattern para servir a SPA, mas excluindo rotas da API e admin
+    re_path(r'^(?!api/|admin/).*$', views.home, name='spa-fallback'),
 ]
